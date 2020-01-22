6dfd779d1d7f12a3e30d86b7c84213fb910cadf9
arigbede emmanuel <Harriemannie@gmail.com>
2020-01-22 12:54:51 +0100

    fix button/links

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true, uniqueness: true
 end
