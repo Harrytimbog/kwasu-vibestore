80b6a19c9bdd15a849e38580bae44368388de258
arigbede emmanuel <Harriemannie@gmail.com>
2020-02-02 23:02:14 +0100

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,8 +2,6 @@ Rails.application.configure do
   # Verifies that versions and hashed value of the package contents in the project's package.json
   config.webpacker.check_yarn_integrity = false
 
-  config.active_storage.service = :amazon
-
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
