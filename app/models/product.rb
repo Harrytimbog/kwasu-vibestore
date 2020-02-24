6a71cd3aa6abbb2636dc7b98d64dbd8361b38e08
arigbede emmanuel <Harriemannie@gmail.com>
2020-02-24 11:47:46 +0100

    make product image display on each product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+  has_one_attached :photo
   has_many :reviews, dependent: :destroy
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true, uniqueness: true
